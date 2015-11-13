0af224ef74687c392f4dd95e8bd8b004b5d5b911
miya0001 <miya@wpist.me>
2015-11-13 22:13:45 +0900

    Revert "add 3d effect"
    This reverts commit 1014bb7f3eae9e79f2356c58a6d219f6d758695a.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,11 @@
 'use strict';
 
 var gulp = require( 'gulp' );
-var replace = require( 'gulp-replace' );
-var concat = require( 'gulp-concat' );
-var uglify = require( 'gulp-uglify' );
-var rename = require( 'gulp-rename' );
-var minifyCss = require( 'gulp-minify-css' );
-var download = require( 'gulp-download' );
-
-gulp.task( 'download', function () {
-	return download( [
-			'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/Detector.js',
-			'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/controls/DeviceOrientationControls.js',
-		] )
-		.pipe( gulp.dest( 'src' ) );
-});
+var replace = require('gulp-replace');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task( 'twentythirteen_style', function () {
 	return gulp.src( [
@@ -25,18 +16,10 @@ gulp.task( 'twentythirteen_style', function () {
 		.pipe( replace( '#bc360a', '#337ab7' ) )
 		.pipe( replace( '604px', '750px' ) )
 		.pipe( replace( 'italic', 'normal' ) )
-		.pipe( replace( 'images/search-icon.png', '../../twentythirteen/images/search-icon.png' ) )
 		.pipe( rename( {
 			basename: 'twentythirteen',
 			extname: '.css'
 		} ) )
-		.pipe( gulp.dest( 'src' ) );
-} );
-
-gulp.task( 'npm', function () {
-	return gulp.src( [
-			'node_modules/three.js/build/three.js'
-		] )
 		.pipe( gulp.dest( 'src' ) );
 } );
 
@@ -58,17 +41,6 @@ gulp.task( 'css', [ 'twentythirteen_style', 'twitter_bootstrap' ], function () {
 		.pipe( gulp.dest( 'css' ) );
 } );
 
-gulp.task( 'uglify', [ 'download', 'npm' ], function(){
-  return gulp.src( [
-		  'src/*.js'
-	  ] )
-	  .pipe( uglify() )
-	  .pipe( rename( {
-		  extname: '.min.js'
-	  } ) )
-	  .pipe( gulp.dest( 'js' ) );
-} );
-
-gulp.task( 'default', [ 'uglify', 'css' ], function () {
+gulp.task( 'default', [ 'css' ], function () {
 
 } );
