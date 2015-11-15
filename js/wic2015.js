84d69313ac16f5d7174149363147f0a2860d65af
miya0001 <miya@wpist.me>
2015-11-16 05:48:02 +0900

    fix problem with firefox

--- js/wic2015.js
+++ js/wic2015.js
@@ -3,12 +3,21 @@
 	$( custom_background_container ).addClass( 'custom-background-container' );
 	$( 'body' ).append( $( custom_background_container ) );
 	$( custom_background_container ).css( 'background', $( 'body' ).css( 'background' ) );
-	$( custom_background_container ).css( 'height', $( window ).height() + 100 );
-	$( 'body' ).css( 'background', 'none' );
+	$( custom_background_container ).css( 'height', $( window ).height() );
+	//$( 'body' ).css( 'background', 'none' );
+
+	if ( 750 >= $( window ).width() ) {
+		$( custom_background_container ).css( 'height', $( window ).height() + 200 );
+	}
+
+	$( window ).resize( function(){
+		if ( 750 < $( window ).width() ) {
+			$( custom_background_container ).css( 'height', $( window ).height() );
+		}
+	} );
 
 	$( document ).ready( function(){
 		if ( $( '#wpadminbar' ).length ) {
-			console.log( $( '#wpadminbar' ).height() );
 			$( '#navbar' ).css( 'top', $( '#wpadminbar' ).height() );
 		}
 	} );
