ff9ce138d81550e0c12306bfe71d6cdd2ee527ec
miya0001 <miya@wpist.me>
2015-11-20 17:25:11 +0900

    update font-size

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ require_once dirname( __FILE__ ) .'/lib/logo-customizer.php';
 if ( ! isset( $content_width ) )
 	$content_width = 750;
 
-define( 'WIC2015_VERSION', 'v0.1.0b' );
+define( 'WIC2015_VERSION', 'v0.1.1' );
 
 
 add_action( 'after_setup_theme', 'wic2015_a_after_setup_theme_01' );
