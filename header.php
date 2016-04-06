3dbf7a0566015d3f6143cbdefb066f136cc0b4d3
miya0001 <miya@wpist.me>
2016-04-07 08:27:10 +0900

    update textdomain

--- header.php
+++ header.php
@@ -51,8 +51,8 @@
 
 			<div id="navbar" class="navbar">
 				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
-					<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
+					<button class="menu-toggle"><?php _e( 'Menu', 'wic2015' ); ?></button>
+					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'wic2015' ); ?>"><?php _e( 'Skip to content', 'wic2015' ); ?></a>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
 					<?php get_search_form(); ?>
 				</nav><!-- #site-navigation -->
